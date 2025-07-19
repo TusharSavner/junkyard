c2ecd3670213f83d70fdf4845fe2dfa1459a9855
Tushar savner <savnertushar@gmail.com>
2025-07-19 19:14:02 +0530

    Create counting.js
    created the file

---
+++ web_junk/counting/js/counting.js
@@ -0,0 +1,14 @@
+var number = 0
+
+function dec() {
+    number = number + 1;
+    document.getElementById("display").textContent = number
+};
+function res() {
+    number = 0;
+    document.getElementById("display").textContent = number
+};
+function inc() {
+    number = number + 1;
+    document.getElementById("display").textContent = number
+}
